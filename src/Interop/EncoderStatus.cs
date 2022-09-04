37cf0183366cbdd79412658ccaf93d8ffacdab8b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2022-09-04 07:52:54 -0600

    Remove unnecessary usings

--- src/Interop/EncoderStatus.cs
+++ src/Interop/EncoderStatus.cs
@@ -10,12 +10,6 @@
 //
 ////////////////////////////////////////////////////////////////////////
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace JpegXLFileTypePlugin.Interop
 {
     internal enum EncoderStatus : int
