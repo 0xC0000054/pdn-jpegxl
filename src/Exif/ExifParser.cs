cf0b066e5d0bd6c5c3d508175bdd69ef872f158b
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-01-28 03:19:41 -0700

    Bump copyright year

--- src/Exif/ExifParser.cs
+++ src/Exif/ExifParser.cs
@@ -3,7 +3,7 @@
 // This file is part of pdn-jpegxl, a FileType plugin for Paint.NET
 // that loads and saves JPEG XL images.
 //
-// Copyright (c) 2022 Nicholas Hayes
+// Copyright (c) 2022, 2023 Nicholas Hayes
 //
 // This file is licensed under the MIT License.
 // See LICENSE.txt for complete licensing and attribution information.
