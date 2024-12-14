d3c2260718a9ba1bae30076ac4dab46eef0db71d
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-12-14 13:12:41 -0700

    Add a missing file header

--- src/Interop/JpegXLImageFormat.cs
+++ src/Interop/JpegXLImageFormat.cs
@@ -1,8 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿////////////////////////////////////////////////////////////////////////
+//
+// This file is part of pdn-jpegxl, a FileType plugin for Paint.NET
+// that loads and saves JPEG XL images.
+//
+// Copyright (c) 2022, 2023, 2024 Nicholas Hayes
+//
+// This file is licensed under the MIT License.
+// See LICENSE.txt for complete licensing and attribution information.
+//
+////////////////////////////////////////////////////////////////////////
 
 namespace JpegXLFileTypePlugin.Interop
 {
