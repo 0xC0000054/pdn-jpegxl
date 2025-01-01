9c92f27b7d7f9ad2e5792eabfe18e4b55b6a910d
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2024-12-31 22:44:33 -0700

    Set uses_original_profile to false for lossy encoding
    The plugin was previously setting it to true when an ICC profile was
    present, but this behavior put libjxl into an unsupported mode
    (RGB lossy) and caused blocking artifacts on some images.
    
    See the following discussion on the JPEG XL Discord for more details:
    https://discord.com/channels/794206087879852103/804324493420920833/1323849208318529556
    
    Closes #6

--- src/JxlFileTypeIO/Encoder/JxlEncoder.cpp
+++ src/JxlFileTypeIO/Encoder/JxlEncoder.cpp
@@ -211,7 +211,7 @@ EncoderStatus EncoderWriteImage(
         basicInfo.ysize = bitmap->height;
         basicInfo.bits_per_sample = 8;
         basicInfo.exponent_bits_per_sample = 0;
-        basicInfo.uses_original_profile = options->lossless || metadata && metadata->iccProfileSize > 0;
+        basicInfo.uses_original_profile = options->lossless;
         basicInfo.alpha_exponent_bits = 0;
         basicInfo.alpha_premultiplied = false;
 
